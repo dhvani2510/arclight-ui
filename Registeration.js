5f3f71679270d7016f6dc3c7d4410b0436a02665
Riddhi <riddhijobanputra5@gmail.com>
2023-07-04 20:42:12 -0400

    Create Registeration File

---
+++ Registeration.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+export default function Registeration()
+{
+    return (
+        <><View>
+       
+      <Text>Hi</Text>  
+        </View></>
+      );
+}
