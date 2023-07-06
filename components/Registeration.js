9210d2ab69c0034662ab16dce592a174285f308c
Riddhi <riddhijobanputra5@gmail.com>
2023-07-05 22:45:21 -0400

    Registeration Page Initial Design

--- components/Registeration.js
+++ components/Registeration.js
@@ -1,24 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
-export default function Registeration()
-{
-    return (
-      <><>
-        <TextInput
-          style={styles.input}
-          value={username}
-          placeholder={"Username"}
-          onChangeText={(text) => setUsername(text)}
-          autoCapitalize={"none"} />
-        <TextInput
-          style={styles.input}
-          value={password}
-          placeholder={"Password"}
-          secureTextEntry
-          onChangeText={(text) => setPassword(text)} />
-        <Button title={"Sign Up"} onPress={() => doUserRegistration()} />
-      </><><View>
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+export default function Registeration() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');	
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-        <Text>Hi</Text>
-      </View></></>
-      );
-}
+  const handleRegister = () => {
+    if (name && email && password && confirmPassword) {
+      if (password === confirmPassword) {
+        // Perform registration logic here
+        Alert.alert('Registration Successful!');
+      } else {
+        Alert.alert('Passwords do not match');
+      }
+    } else {
+      Alert.alert('Please fill in all fields');
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Name"
+        value={name}
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <TextInput
+        placeholder="Confirm Password"
+        secureTextEntry
+        value={confirmPassword}
+        onChangeText={text => setConfirmPassword(text)}
+      />
+      <Button title="Register" onPress={handleRegister} />
+    </View>
+  );
+};
