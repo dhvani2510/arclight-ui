4688ff79b9bf38982e35fe72e36adec49066d38b
Sheth Dhvani <dsheth@tavisca.in>
2023-07-22 15:01:16 -0400

    Changes to load category content

--- Screen/DrawerScreens/Scores.js
+++ Screen/DrawerScreens/Scores.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useEffect } from 'react';
 import {View, Text, SafeAreaView, Image} from 'react-native';
 
 const Scores = () => {
+    useEffect(() => {
+        fetchScores();
+      }, []);
+  
+      const fetchScores = async () => {
+          const access_token = await AsyncStorage.getItem('access-token');
+          console.log(access_token);
+          return fetch('https://arclight.iverique.com/api/v1/quiz/scores/' , {
+              method: 'GET',
+              headers: {
+                  'Authorization': 'Bearer '+ access_token
+              },
+              })
+              .then(data => {
+                console.warn(JSON.stringify(data));
+                data.json();
+              })
+              .then(response => {
+                console.warn(JSON.stringify(response.data));
+                  setQuestions(response.data);
+                  setLoading(false);
+              });
+      };
     return (
         <View style={{padding: 16}}>
             <Text>Scores</Text>
         </View>
-        
     );
 };
 
